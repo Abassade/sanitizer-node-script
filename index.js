d423da7a50fed724049af97ab5688298e6b9d9b9
Abass <abs4real16.ma@gmail.com>
2019-02-19 10:18:51 +0100

    prepare to go remote

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const slug = require('slug')
 
 
 app.use(require('./routes/index'));
+
+console.log((slug('abass makinde')));
 
 app.listen(port, ()=>{
     console.info(`App is listening on port ${port}`);
